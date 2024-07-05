a4ee9b65237aecdeb2eee61b6c7e4f1b114cc4dc
Björn Büttner <Idrinth@users.noreply.github.com>
2024-07-05 05:05:19 +0200

    better calculation for absolute position

--- resources/scripts.js
+++ resources/scripts.js
@@ -31,7 +31,7 @@ window.imm = {
             const deltaY = Math.abs(targetTop - originTop);
             const deltaHyp = Math.sqrt(deltaX * deltaX + deltaY * deltaY + 2 * deltaX * deltaY);
             const degrees = Math.asin(deltaX / deltaHyp)+90;
-            arrow.setAttribute('style', 'transform: rotate(' + degrees + 'deg);height: '+deltaHyp+'px;transform-origin: '+originRight+'px '+originTop+'px;');
+            arrow.setAttribute('style', 'transform: rotate(' + degrees + 'deg);height: '+deltaHyp+'px;left: '+originRight+'px;top: '+Math.max(originTop, originTop)+'px;transform-origin: 0px '+(source.height/2)+'px;');
         }
     },
     getBoundingClientRect(element) {
