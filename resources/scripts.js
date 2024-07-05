a9f78298adce6d48cca7b6c2b0d40271522cfa9f
Björn Büttner <Idrinth@users.noreply.github.com>
2024-07-05 09:23:12 +0200

    fix transform origin

--- resources/scripts.js
+++ resources/scripts.js
@@ -39,7 +39,7 @@ window.imm = {
                 arrow.setAttribute('style', 'transform: rotate(90deg);height: '+deltaY+'px;left: '+originRight+'px;top:'+originTop+'px;');
             } else {
                 const deltaHyp = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-                const degrees = Math.atan(deltaX / deltaY) * 120 + 90;
+                const degrees = Math.atan(deltaX / deltaY) * 60 + 90;
                 arrow.setAttribute('style', 'transform: rotate(' + degrees + 'deg);height: ' + deltaHyp + 'px;left: ' + originRight + 'px;top:' + originTop + 'px;');
             }
             pos++;
