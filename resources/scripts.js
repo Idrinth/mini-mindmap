f49e7e0e64051f6d1a0e084dc7b6fdfa2dd6bbf1
Björn Büttner <Idrinth@users.noreply.github.com>
2024-07-05 05:29:49 +0200

    better calculation for absolute position

--- resources/scripts.js
+++ resources/scripts.js
@@ -27,8 +27,8 @@ window.imm = {
             const originTop = source.top + source.height/2;
             const targetLeft = target.left - 10;
             const targetTop = target.top + source.height/2;
-            const deltaX = originRight - targetLeft;
-            const deltaY = originTop - targetTop;
+            const deltaX = targetLeft - originRight;
+            const deltaY = targetTop - originTop;
             const deltaHyp = Math.sqrt(deltaX * deltaX + deltaY * deltaY + 2 * deltaX * deltaY);
             const degrees = Math.asin(deltaX / deltaHyp)+90;
             arrow.setAttribute('style', 'transform: rotate(' + degrees + 'deg);height: '+deltaHyp+'px;left: '+(originRight+deltaX/2)+'px;top: '+(Math.min(originTop, originTop)-deltaY)+'px;border-right: 1px solid darkgreen; transform-origin: '+originRight+'px '+originTop+'px;');
