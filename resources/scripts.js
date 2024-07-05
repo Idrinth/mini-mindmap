241b3a7cad971cfd83d203e99646230761bad6f9
Björn Büttner <Idrinth@users.noreply.github.com>
2024-07-05 09:21:50 +0200

    fix transform origin

--- resources/scripts.js
+++ resources/scripts.js
@@ -33,11 +33,13 @@ window.imm = {
             const deltaX = targetLeft - originRight;
             const deltaY = targetTop - originTop;
             arrow.setAttribute('data-child-pos', pos);
+            arrow.setAttribute('data-x', deltaX);
+            arrow.setAttribute('data-y', deltaY);
             if (deltaY < 1 && deltaY > -1) {
                 arrow.setAttribute('style', 'transform: rotate(90deg);height: '+deltaY+'px;left: '+originRight+'px;top:'+originTop+'px;');
             } else {
                 const deltaHyp = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-                const degrees = Math.atan(deltaX / deltaY) * 180 + 90;
+                const degrees = Math.atan(deltaX / deltaY) * 120 + 90;
                 arrow.setAttribute('style', 'transform: rotate(' + degrees + 'deg);height: ' + deltaHyp + 'px;left: ' + originRight + 'px;top:' + originTop + 'px;');
             }
             pos++;
