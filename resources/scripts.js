052649d934ba14649810b087077e3a08e2bb02c3
Björn Büttner <Idrinth@users.noreply.github.com>
2024-07-05 04:09:42 +0200

    draw arrow svg

--- resources/scripts.js
+++ resources/scripts.js
@@ -27,11 +27,11 @@ window.imm = {
             const originTop = source.top;
             const targetLeft = target.left;
             const targetTop = target.top;
-            const deltaX = originRight - targetLeft;
-            const deltaY = originTop - targetTop;
+            const deltaX = targetLeft - originRight;
+            const deltaY = targetTop - originTop;
             const deltaHyp = Math.sqrt(deltaX * deltaX + deltaY * deltaY + 2 * deltaX * deltaY);
             const degrees = Math.asin(deltaX / deltaHyp)*360;
-            arrow.setAttribute('style', 'transform: rotate(' + degrees + 'deg);height: '+deltaHyp+'px;width: 1em;left: '+originRight+'px;bottom: '+source.bottom+'px;transform-origin: '+originRight+'px '+source.bottom+'px;');
+            arrow.setAttribute('style', 'transform: rotate(' + degrees + 'deg);height: '+deltaHyp+'px;left: '+originRight+'px;bottom: '+source.bottom+'px;transform-origin: '+originRight+'px '+source.bottom+'px;');
         }
     },
     mouse(e) {
