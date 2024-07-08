4becdc97066afe9c66aadbfe6a628f2398475adf
Björn Büttner <Idrinth@users.noreply.github.com>
2024-07-08 16:24:13 +0200

    arrow adjustments - smaller arrows

--- resources/scripts.js
+++ resources/scripts.js
@@ -36,11 +36,11 @@ window.imm = {
             arrow.setAttribute('data-x', deltaX);
             arrow.setAttribute('data-y', deltaY);
             if (deltaY < 1 && deltaY > -1) {
-                arrow.setAttribute('style', 'transform: rotate(270deg);height: '+deltaX+'px;left: '+originRight+'px;top:'+(originTop+20)+'px;');
+                arrow.setAttribute('style', 'transform: rotate(270deg);background-size: 20px '+deltaX+'px;height: '+deltaX+'px;left: '+originRight+'px;top:'+(originTop+10)+'px;');
             } else {
                 const deltaHyp = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
                 const degrees = Math.tan(deltaY / deltaX)/Math.PI * 120 + 90 + 180;
-                arrow.setAttribute('style', 'transform: rotate(' + degrees + 'deg);height: ' + deltaHyp + 'px;left: ' + originRight + 'px;top:' + (originTop+29) + 'px;');
+                arrow.setAttribute('style', 'transform: rotate(' + degrees + 'deg);height: ' + deltaHyp + 'px;background-size: 20px ' + deltaHyp + 'px;left: ' + originRight + 'px;top:' + (originTop+10) + 'px;');
             }
             pos++;
         }
